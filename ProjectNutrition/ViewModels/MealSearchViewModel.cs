2ccc75f45eab31463b987ab3882177374b8d2c74
Andrej Nenadić <andrejnana445@gmail.com>
2024-01-15 19:09:06 +0100

    Started working on MealSearchView, so far it just displays all created meals with no way to actually search or sort

---
+++ ProjectNutrition/ViewModels/MealSearchViewModel.cs
@@ -0,0 +1,20 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using ProjectNutrition.Database;
+using ProjectNutrition.Models;
+using System.Collections.ObjectModel;
+
+namespace ProjectNutrition.ViewModels
+{
+    public partial class MealSearchViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private ObservableCollection<Meal> meals = null!;
+
+        private readonly DataContext context;
+        public MealSearchViewModel(DataContext context)
+        {
+            this.context = context;
+            Meals = [.. context.Meals];
+        }
+    }
+}
---
+++ ProjectNutrition/Views/MealSearchView.xaml.cs
@@ -0,0 +1,9 @@
+namespace ProjectNutrition.Views;
+
+public partial class MealSearchView : ContentView
+{
+	public MealSearchView()
+	{
+		InitializeComponent();
+	}
+}
